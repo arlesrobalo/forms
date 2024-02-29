daf89e9f17fb1e17056bda42ddfed7b1e3a8690f
arlesrobalo <130885359+arlesrobalo@users.noreply.github.com>
2024-02-29 20:15:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-const form = document.forms['submit-to-google-sheet'];
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  fetch('SUA_URL_DA_WEB_APP', {
-    method: 'POST',
-    body: new FormData(form),
-  })
-  .then(response => response.json())
-  .then(response => alert('Formulário enviado com sucesso!'))
-  .catch(error => console.error('Erro ao enviar o formulário:', error));
-});
+ const form = document.forms['submit-to-google-sheet'];
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch('https://script.google.com/macros/s/AKfycbx56V6WtCIq2KYc24dqw1kJxZOwOEqEeuNR9MhhahcPwbFnzJW4lm1xIBAJnQ7l5Ll7/exec', {
+      method: 'POST',
+      body: new FormData(form),
+    })
+    .then(response => response.json())
+    .then(response => alert('Formulário enviado com sucesso!'))
+    .catch(error => console.error('Erro ao enviar o formulário:', error));
+  });
